5b6b79221789ea3f18cfa5ed9a441fc80cfc83f9
Glitch (advaithbot) <none>
2018-01-21 00:16:21 +0000

    ☎️🎉 Updated with Glitch

---
+++ commands/servers.js
@@ -0,0 +1,9 @@
+exports.name = "servers"
+exports.level = 10; 
+
+
+exports.command = function(client, message) {
+
+  message.channel.send(":ok_hand::skin-tone-4:  Killing the bot...")
+
+}
--- functions/boot/boot.js
+++ functions/boot/boot.js
@@ -4,6 +4,28 @@ exports.delay = 0;
 
 exports.function = function(client) {
 
-  console.log("ran boot function")
+  const snekfetch = require('snekfetch') //for discordbots.org stats
+  
+  
+  //runs every hour, used for getting server count for stuff
+setInterval(() => {
+  //updates discordbots.org server count
+  snekfetch.post(`https://discordbots.org/api/bots/398690824721924107/stats`)
+    .set('Authorization', process.env.DBL_TOKEN)
+    .send({ server_count: client.guilds.size })
+    .then(() => console.log('Updated discordbots.org stats.'))
+    .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
+  
+  //updates playing status: Watching x servers
+  client.user.setPresence({
+          status: "online",
+          activity: {
+            name: `${client.guilds.size} servers`,
+            type: 3,
+          }
+        })
+}, 3600000)
 
+    console.log("ran boot function")
+  
 }
