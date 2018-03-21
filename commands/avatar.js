e8ef44bbfee231e87449368a2e8887d2f9d070e6
KiraDev <justolixr@gmail.com>
2018-03-20 20:27:28 -0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,10 +3,10 @@ exports.level = 0;
 
 
 exports.command = function(client, message) {
-    let avatar = msg.mentions.users.size ? msg.mentions.users.first().avatarURL : msg.author.avatarURL;
-    if (msg.mentions.users.size > 0) {
-    msg.channel.send(`Avatar for, **${msg.mentions.users.first().username}:**\n${avatar}`);
+    let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL : message.author.avatarURL;
+    if (message.mentions.users.size > 0) {
+    message.channel.send(`Avatar for, **${message.mentions.users.first().username}:**\n${avatar}`);
   } else {
-    msg.channel.send(`Avatar for, **${msg.author.username}:**\n${avatar}`);
+    message.channel.send(`Avatar for, **${message.author.username}:**\n${avatar}`);
     }
 };
